2aada8bd577282be4a98873d03c35f80b58956d9
issawiold <omar.m.issawi.95@gmail.com>
2023-09-04 16:45:03 +0000

    work

---
+++ src/main/java/com/example/songr/controller/Album.java
@@ -0,0 +1,37 @@
+package com.example.songr.controller;
+
+public class Album {
+    private String title;
+    private String artist;
+    private int songCount;
+    private int length;
+    private String imageUrl;
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
